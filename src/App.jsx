8f6c9b328083ee21ce51749799e7b0809d84a586
Avinash kumar <akavinashsingh@gmail.com>
2025-11-04 21:29:33 +0530

    integrating api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+
 import Button from '@mui/material/Button';
+import SearchBox from './SearchBox';
 
 
 
@@ -10,7 +8,7 @@ function App() {
   return (
     <>
       <h1>Material UI Demo </h1>
-      <Button variant="contained">Clicke Me</Button>
+      <SearchBox/>
     </>
   );
 }
---
+++ src/SearchBox.jsx
@@ -0,0 +1,55 @@
+import TextField from "@mui/material/TextField";
+import Button from '@mui/material/Button';
+import "./SearchBox.css";
+import { useState } from "react";
+
+export default function SearchBox() {
+    let [city,setCity] = useState("");
+const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "8bd445a99e282d4e09196b15992ee230";
+
+let getWeatherInfo = async()=>{
+ let response =  await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+let jsonResponse= await response.json();
+
+let result = {
+    temp: jsonResponse.main.temp,
+    tempMin: jsonResponse.main.temp_min,
+    tempMax: jsonResponse.main.temp_max,
+    humidity: jsonResponse.main.humidity,
+    feelsLike: jsonResponse.main.feels_like,
+    weather: jsonResponse.weather[0].description,
+};
+
+};
+
+
+    
+
+let handleChange = (evt)=>{
+   setCity(evt.target.value);
+   
+};
+
+let handleSubmit = (evt) =>{
+    evt.preventDefault();
+    console.log(city);
+    setCity("");
+    getWeatherInfo();
+};
+
+
+
+  return (
+    <div className="SearchBox">
+      <h3>Search for the Weather</h3>
+      <form onSubmit={handleSubmit}>
+        <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange}/>
+        <br /><br />
+       <Button variant="contained" type="submit">
+        Search
+      </Button>
+      </form>
+    </div>
+  );
+}
